56be57f658e2c24302e04b5824dec9a94daa7eea
Björn Gräbe <bjoerng@users.noreply.github.com>
2021-03-01 21:37:17 +0100

    Update clusterssh-on-region.el

--- clusterssh-on-region.el
+++ clusterssh-on-region.el
@@ -1,7 +1,7 @@
 (defun clusterssh-on-region ()
   "Sent region to clusterssh"
   (interactive)
-  (let ((proc (apply 'start-process "clusterssh-process" "clusterssh-buffer" "clusterssh" (get-host-in-region-as-list))))))
+  (apply 'start-process "clusterssh-process" "clusterssh-buffer" "clusterssh" (get-host-in-region-as-list)))
 
 (defun get-host-in-region-as-list ()
   (let* ((region-string (buffer-substring-no-properties (region-beginning) (region-end))))
