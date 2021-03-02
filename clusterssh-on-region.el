c1518ba1686d2eb48a092063f492afd100b418ad
Björn Gräbe <bjoerng@users.noreply.github.com>
2021-03-02 23:11:31 +0100

    Update clusterssh-on-region.el

--- clusterssh-on-region.el
+++ clusterssh-on-region.el
@@ -3,6 +3,8 @@
   (interactive)
   (apply 'start-process "clusterssh-process" "clusterssh-buffer" "clusterssh" (get-host-in-region-as-list)))
 
+
+
 (defun get-host-in-region-as-list ()
   (let* ((region-string (buffer-substring-no-properties (region-beginning) (region-end))))
     (split-string region-string)))
@@ -11,9 +13,10 @@
 
 (defun clusterssh-on-rectangle ()
   (interactive)
-    (let* ((region-strings  (extract-rectangle (region-beginning) (region-end))))
-      (mapcar (lambda (string-val) (set-text-properties 0 (length string-val) nil string-val)) region-strings)
+    (let* ((rectangle-strings  (extract-rectangle (region-beginning) (region-end))))
+      (mapcar (lambda (string-val) (set-text-properties 0 (length string-val) nil string-val))
+	      rectangle-strings)
       (apply 'start-process "clusterssh-process" "clusterssh-buffer" "clusterssh"
-	     (mapcar #'string-trim region-strings))))
+	     (mapcar #'string-trim rectangle-strings))))
 
 (global-set-key (kbd "C-c r") 'clusterssh-on-rectangle)
